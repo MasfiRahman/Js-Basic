a861a77af88e10e8739c6e9bbb27233bdb78f883
MasfiRahman <masfirahman602@gmail.com>
2025-11-24 01:02:16 +0600

    It's the file of Some details an example of Keyword,DataTypes and Comment

---
+++ Keyword,DataType and Comment.js
@@ -0,0 +1,23 @@
+//you can run this program by using the command =>   node program2.js
+
+// keywords -> reserved words for specific reasons. example -> for,while, do while, if, else, else if, break, try,catch, function, var, const, let etc.
+
+// data type
+// primitive data type - String, Number (int,float,double), Boolean, Undefined, Null
+// Reference data type - Array, Object.
+// use typeof() to check the data type
+// comment -> single line comment, double line comment
+
+//This is a single line comment
+
+/*
+This is 
+multiple line
+comment
+*/
+
+console.log(typeof 123);
+console.log(typeof 123.5);
+console.log(typeof "123");
+console.log(typeof true);
+console.log(typeof false);
