bfdc406f6c105f8e271a5a90843fdbafeae8df83
MasfiRahman <masfirahman602@gmail.com>
2025-11-23 20:24:17 +0600

    Creating External Js page for External Javascript for ongoing website good it's conneting to Html main page in script tag part and then it's working

--- How to add JavaScript in Html.js
+++ How to add JavaScript in Html.js
@@ -1,2 +1,2 @@
-
+//It's a part of external Javascript and this page to write many javascript code are writing in manually without any function ,there is no fuction needed in external Hs page and external javascript page use for smoothing website and good scrolling and refreshment to correcting fresh loook way to website
         alert("Hello");
