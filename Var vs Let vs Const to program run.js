150c9d3ff2ad2e34840b795b06a2560092a7e0fc
MasfiRahman <masfirahman602@gmail.com>
2025-11-24 17:09:12 +0600

    Adding New file for Variable and Let and Constant for good knowing about js and let and const use all time var use not all time beacause it is backdated

---
+++ Var vs Let vs Const to program run.js
@@ -0,0 +1,19 @@
+// Var vs Let vs Const
+
+
+//Var Keyword as a Old Keyword it's called to override character and ut's called to redeclare
+//var fullName='Masfi';
+//var fullName='Rahman';
+//console.log(fullName);
+
+
+
+//let fullName='Masfi';
+//let fullName='rahman';// Using Let Keyword are not repetedly use and it's use show something wrong and redeclare variable not allowed in let Keyword and also use then it's variable name is change ad it's time to use and next time is changable 
+//const fullName='rahman';// Using Const Keyword are not repetedly use and it's use show something wrong and redeclare variable not allowed in const Keyword and also use then it's variable name is change and it's one time used and next time is not changable
+const lastName='rahman';
+console.log(fullName);
+// Js file run in terminal to use first use code :
+//node -v (for version check)
+//node filename/filename.js
+
