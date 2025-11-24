026851106c0d1e3cd5a41288ff7cd7f2b05e5a42
MasfiRahman <masfirahman602@gmail.com>
2025-11-24 18:43:31 +0600

    Added Personal Budget Tracker App with Phase 1

---
+++ Personal Budget Tracker App Challange Phase 1.js
@@ -0,0 +1,16 @@
+//! 🛠 Personal Budget Tracker Challenge (Phase 1 - print with Console)
+// Challenge Overview:Create a basic personal budget tracker that allows users to  displays their name, income amount and expenses amount using the console. Later, we will extend this project by adding variables, operators, functions, arrays, objects, and DOM manipulation.
+// 📌 Challenge Requirements:
+/*
+Use console.log() to:
+Display the user’s name.
+Display the income, expenses.
+Format the output neatly.
+explain with comment.
+*/
+
+// solution of Challenge 1
+console.log('Personal Budget Tracker App');
+console.log('Masfi Rahman');
+console.log('3750.25');//My income
+console.log('2580.0');//My expense
