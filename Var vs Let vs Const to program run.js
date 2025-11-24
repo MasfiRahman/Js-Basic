2d4b57aff21cbbefc94c6fda998a075d84aa99c4
MasfiRahman <masfirahman602@gmail.com>
2025-11-24 17:17:42 +0600

    Some changes in coment out to for showing errror and fix this

--- Var vs Let vs Const to program run.js
+++ Var vs Let vs Const to program run.js
@@ -12,7 +12,7 @@
 //let fullName='rahman';// Using Let Keyword are not repetedly use and it's use show something wrong and redeclare variable not allowed in let Keyword and also use then it's variable name is change ad it's time to use and next time is changable 
 //const fullName='rahman';// Using Const Keyword are not repetedly use and it's use show something wrong and redeclare variable not allowed in const Keyword and also use then it's variable name is change and it's one time used and next time is not changable
 const lastName='rahman';
-console.log(fullName);
+console.log(lastName);
 // Js file run in terminal to use first use code :
 //node -v (for version check)
 //node filename/filename.js
