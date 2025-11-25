8edfe4c789d94aa5092f92b78b69f8236d52a0de
MasfiRahman <masfirahman602@gmail.com>
2025-11-25 23:29:50 +0600

    Adding New Js File as a Ternary Operator and it's funcyioning as a Single condition and Multiple condition

---
+++ Ternary Operator.js
@@ -0,0 +1,5 @@
+var number = Number(prompt("Enter a number "));
+
+number > 0 ? console.log("positive") : console.log("negative");//Single condition positive ,negative 
+var result = number>0 ? "positive" : number<0 ? "Negative" : "zero";//Multiple condition positive ,negative zero
+console.log(result)
