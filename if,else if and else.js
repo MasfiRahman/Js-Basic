e48db481a1e03b2062caf89949d2fa3ea5c2c670
MasfiRahman <masfirahman602@gmail.com>
2025-11-25 19:09:08 +0600

    Adding New Js File Conditional Statement like If,Else-If,ELse

---
+++ if,else if and else.js
@@ -0,0 +1,45 @@
+var num = 5;
+
+if (num%2==0){
+    console.log("Even");
+}else if (num%2!=0){
+    console.log("Odd");
+}else{
+    console.log("Zero");
+}
+
+
+// conditional control statement -> if, else if, else, switch
+
+// a program that will find a large number between 2 numbers
+var num1 = parseInt(prompt("Enter first numebr : "));
+var num2 = parseInt(prompt("Enter second numebr : "));
+
+// first method - using only if
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+}
+if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+}
+if (num1 == num2) {
+  console.log("Equal numbers");
+}
+
+// second method - more efficient than first method
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+} else if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+} else if (num1 == num2) {
+  console.log("Equal numbers");
+}
+
+// third method - more efficient than first and second method
+if (num1 > num2) {
+  console.log("Large Number is : " + num1);
+} else if (num1 < num2) {
+  console.log("Large Number is : " + num2);
+} else {
+  console.log("Equal numbers");
+}
