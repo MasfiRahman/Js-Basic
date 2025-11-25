2ab0461b5c96b7c86b87f0a76d2e9c32a938eae8
MasfiRahman <masfirahman602@gmail.com>
2025-11-25 07:05:57 +0600

    Adding Task 1 for Basic Print type

---
+++ Task1 for Basic Js.js
@@ -0,0 +1,5 @@
+/* Program for Task 1*/
+// A program to print your details info
+document.write("Name : Masfi Rahman <br/>");
+document.write("Nationality : Bangladeshi <br/>");
+document.write("Email : masfirahman602@gmail.com");
