3f15156b7cf779d8ba9b00a1e747b9c2365901f0
MasfiRahman <masfirahman602@gmail.com>
2025-11-25 11:20:06 +0600

    Adding New File Area of Shapes and Formula of Different Shapes are mentioning in this Js file

---
+++ Area of Variouse Shapes.js
@@ -0,0 +1,31 @@
+//getting user input for base
+var base = parseFloat(prompt("Enter Base = "));
+
+//getting user input for height
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = base*height;
+document.write("Area = "+area);
+
+//Square Formula : base * height
+//Rectangle Formula : base * height
+//Triangle Formula : 0.5 * base * height
+//Trapizium Formula : ((a+b) * height)/2
+//Parallelogram Formula : base * height
+//Rombus Formula : (length * height)/2
+//Kite Formula :   (length * height)/2
+
+// Area of various shapes
+//Area of triangel
+
+//getting user input for base
+var base = parseFloat(prompt("Enter Base = "));
+
+//getting user input for height
+var height = parseFloat(prompt("Enter Height = "));
+
+//calculating area
+var area = 0.5 * base * height;
+
+//printing area
+document.write("Area of triangle = " + area);
