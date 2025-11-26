6659dbf9b6ccbe977dc1c710fded7c1ecfa901ee
MasfiRahman <masfirahman602@gmail.com>
2025-11-26 22:55:31 +0600

    Adding New Js File of 2D Array is part of DSA

---
+++ 2D Array and topic os DSA.js
@@ -0,0 +1,14 @@
+//2D Array as syntax of [row][column]
+
+var playersinfo = [
+    ["Sakib",95],
+    ["Mashrafe",40],
+    ["Mushfique",20],
+    ["Tamim",180],
+    ["Miraz",30]
+]
+for(var x= 0;x<5;x++){
+    for(var y =0;x<2;y++){
+        console.log(playersinfo[x][y]);
+    }
+}
