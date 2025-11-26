548294cd4b895b357f40ac7c70e33bfe76b3126c
MasfiRahman <masfirahman602@gmail.com>
2025-11-26 23:53:54 +0600

    Guessing game is fun of programming and it's contribution to loop,function,condition,variables

---
+++ Guessing Game.js
@@ -0,0 +1,31 @@
+/* Guessing Game */
+//Guess a number from 1 to 5
+//Generate a random number between 1 to 5
+//If the guess number matches the random number;then show message won else lost
+//Run the games for 5 times
+//Show the number of wons And losts
+var numberOfWins = 0
+var numberOfLost = 0
+
+// run the program for 5 times 
+for (var i = 1; i <= 5; i++) {
+
+    // Guess a number from 1 to 5
+    var guessNumber = parseInt(prompt("Guess a number from 1 to 5 : "))
+
+    // Generate a random number between 1 to 5
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+    //check condition
+
+    if (guessNumber == randomNumber) {
+        numberOfWins++;
+        console.log("You have won ")
+    }
+    else {
+        numberOfLost++;
+        console.log("You have lost. Random number was : " + randomNumber)
+    }
+}
+console.log("Total win = " + numberOfWins)
+console.log("Total lost = " + numberOfLost)
