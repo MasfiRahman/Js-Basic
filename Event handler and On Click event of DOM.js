a636dceecc3154d5ca1fe6d99670e4f9b7dda113
MasfiRahman <masfirahman602@gmail.com>
2025-11-27 18:06:17 +0600

    Adding New Js and Html file of Event Handler and On Click event of DOM

---
+++ Event handler and On Click event of DOM.js
@@ -0,0 +1,33 @@
+/*function myMesseage1(){
+            alert('I am a Button 1');
+        }
+*/
+/*function myMesseage2(){
+            alert('I am a Button 2');
+} */       
+/*function myMesseage3(){
+            alert('I am a Paragraph');
+}*/
+function myMesseage1(){
+            document.querySelector("#para1").innerHTML = "I am a Button 1";
+        }
+function myMesseage2(){
+            document.querySelector("#para1").innerHTML = "I am a Button 2";
+}       
+function myMesseage3(){
+            document.querySelector("#para1").innerHTML = "I am a Paragraph";
+}
+
+
+function myPicture1(){
+          document.querySelector("#myImage").src="Html-logo.jpg";
+        }
+function myPicture2(){
+            document.querySelector("#myImage").src="Js-logo.jpg";
+} 
+
+
+
+
+
+
