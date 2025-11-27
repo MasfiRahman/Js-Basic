0c58ef358832d42ef011e7c3f513e4bba9cc1fa8
MasfiRahman <masfirahman602@gmail.com>
2025-11-27 19:25:59 +0600

    Adding New Js file of Traversing and Manipulating of DOM

---
+++ Traversing and Manipulating of DOM.js
@@ -0,0 +1,23 @@
+/* 
+DOM represents html and xml like a tree structure
+  Manipulating - creating,inserting and removing elements
+  Traversing - selecting an elements from other elements
+
+  Selecting of DOM elements
+  1.getElementById()
+  2.getElementsByClassName()
+  3.getElementsByTagName()
+  4.querySelector()
+  5.querySelectorAll()
+
+  Direction of Traversing 
+  1.Downloads
+    a. querySelector()/querySelectorAll()
+    b.children
+  2.Upwards
+    a.parentElement 
+  3.Sideways
+    a.nextElementSibling,
+      previousElementSibling
+    b.parentElement+children+index
+*/         
