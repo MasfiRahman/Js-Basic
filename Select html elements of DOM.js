f235bb80d9ef5852a5b0ad2eddecccd76a65ba96
MasfiRahman <masfirahman602@gmail.com>
2025-11-27 13:59:28 +0600

    Deleting JQuery in SElect Html elements od DOM js file

--- Select html elements of DOM.js
+++ Select html elements of DOM.js
@@ -35,23 +35,3 @@ console.log(h1)
 // finding element by tagName
 var p = document.getElementsByClassName("para")[0];
 console.log(p)
-
-
-// finding element by querySelector
-var q1 = document.querySelector("#heading2");
-console.log(q1)
-
-//it will get the first element of the h1 elements
-var q2 = document.querySelector("h1");
-console.log(q2)
-
-//it will get the first element of the para class
-var q3 = document.querySelector(".para");
-console.log(q3)
-
-var list = document.querySelector(".my-div li");
-console.log(list)
-
-// finding element by querySelectorAll
-var q4 = document.querySelectorAll(".para");
-console.log(q4)
