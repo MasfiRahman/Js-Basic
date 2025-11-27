52396c48c394bf3b6c04e719a9d4994af81229f6
MasfiRahman <masfirahman602@gmail.com>
2025-11-27 13:11:01 +0600

    Adding New Js File of Cookies of API and it's an part Werb Storage API

---
+++ Cookies of API.js
@@ -0,0 +1,6 @@
+document.cookie = "username=masfi; expires= Thurs, 27 Nov 2025 7:08 AM GMT;";
+
+console.log(document.cookie);
+
+/*cookies working with small memory(4KB) and it's working time all part are mentioning otherwise it's not working rightly and it's not working 
+straiaght forward of localStorage and sessionStorage and it's expiray is also mentioning to set*/
