c0b45f18c6527d23e421cdb18b37a2fb660e8800
MasfiRahman <masfirahman602@gmail.com>
2025-11-28 17:56:33 +0600

    Adding new Js dile of Arrow Function part 1of ES6

---
+++ Arrow function  1 of ES6.js
@@ -0,0 +1,70 @@
+// Arrow function vs Traditional function
+
+// demo1 - must use parenthesis for no parameters, but for one parameter its optional
+function display1(){
+    console.log("I am display 1");
+}
+const display2 = () =>{
+    console.log("I am display 2");
+}
+display1();
+display2();
+
+
+
+
+// demo2 - no need to use curly braces if returning or dealing with single statement
+function display3(){ console.log("I am display 3");}
+const display4 = () => console.log("I am display 4");
+display3();
+display4();
+
+
+
+
+// returning value in arrow function - no need to use curly braces if returning or dealing with single statement
+function display5(){ 
+    return "I am display 5";
+}
+const display6 = () => "I am display 6";
+console.log(display5())
+console.log(display6())
+
+
+
+
+// parameters in arrow function 
+function add1(x,y){ 
+    return x+y;
+}
+const add2 = (x,y) => x+y;
+console.log(add1(10,20))
+console.log(add2(20,30))
+
+
+
+
+// Arrow function with map and filter
+var students = [
+    {
+        id : 101,
+        name : 'l',
+        gpa : 3.92
+    },
+    {
+        id : 102,
+        name : 'b ',
+        gpa : 3.12
+    },
+    {
+        id : 103,
+        name : 'R',
+        gpa : 2.92
+    },
+    {
+        id : 104,
+        name : 'L',
+        gpa : 4.92
+    },
+]
+console.log(students);
