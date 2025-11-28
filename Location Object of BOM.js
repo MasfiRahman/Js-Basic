e767efbf8fd0b7b55f9a2bbd2e075c7899f88f53
MasfiRahman <masfirahman602@gmail.com>
2025-11-28 11:54:01 +0600

    Adding New Js file of Location Object of BOM

---
+++ Location Object of BOM.js
@@ -0,0 +1,23 @@
+console.clear();
+
+var locationDiv = document.querySelector(".location-div");
+
+var p1 = locationDiv.children[0];
+p1.textContent = location.href;
+
+var p2 = locationDiv.children[1];
+p2.textContent = location.protocol;
+
+var p3 = locationDiv.children[2];
+p3.textContent = location.hostname;
+
+var p4 = locationDiv.children[3];
+p4.textContent = location.port;
+
+var p5 = locationDiv.children[4];
+p5.textContent = location.pathname;
+
+const visitButton = document.getElementById("visit-button");
+visitButton.addEventListener("click", function () {
+  location.assign("https://w3schoolsua.github.io/js/js_window_en.html#gsc.tab=0");
+});
