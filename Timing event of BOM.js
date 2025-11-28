ee797f69cc9e2bc06c601869b89120fc5a317294
MasfiRahman <masfirahman602@gmail.com>
2025-11-28 12:31:55 +0600

    Adding New Js file of Timing event setInterval(),setTimeout() and clock of BOM

--- Timing event of BOM.js
+++ Timing event of BOM.js
@@ -41,3 +41,27 @@ const message = document.querySelector(".message");
 //     message.textContent = count;
 //   }, 1000);
 // }
+saveButton.addEventListener("click", startClock);
+
+function startClock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  minutes = formatTime(minutes);
+  seconds = formatTime(seconds);
+
+  let time = hours + ":" + minutes + ":" + seconds;
+
+  message.textContent = time;
+
+  setInterval(startClock, 1000);
+}
+
+function formatTime(value) {
+  if (value < 10) {
+    value = "0" + value;
+  }
+  return value;
+}
